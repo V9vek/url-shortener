1a42ff860edf34b71ab3e75c0b610c72c2018812
Vivek Sharma <viveksharma2382000@gmail.com>
2024-12-17 23:31:50 +0530

    feat: dockerized the app

--- src/db/redisClient.js
+++ src/db/redisClient.js
@@ -2,7 +2,7 @@ import { createClient } from "redis";
 
 const redisClient = createClient({
   socket: {
-    host: process.env.REDIS_HOST || "127.0.0.1",
+    host: process.env.REDIS_HOST || "redis",
     port: process.env.REDIS_PORT || 6379,
   },
 });
