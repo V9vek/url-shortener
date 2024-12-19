f6b2d1606e92538590612c284d8e39304809f936
Vivek Sharma <viveksharma2382000@gmail.com>
2024-12-20 01:21:48 +0530

    fix: cloud redis url used

--- src/db/redisClient.js
+++ src/db/redisClient.js
@@ -1,10 +1,11 @@
 import { createClient } from "redis";
 
 const redisClient = createClient({
-  socket: {
-    host: process.env.REDIS_HOST || "redis",
-    port: process.env.REDIS_PORT || 6379,
-  },
+  // socket: {
+  //   host: process.env.REDIS_HOST || "redis",
+  //   port: process.env.REDIS_PORT || 6379,
+  // },
+  url: process.env.REDIS_URL
 });
 
 redisClient.on("error", (err) => console.error("Redist Client Error: ", err));
